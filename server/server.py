8bcb94b08e4fe56fd5d8993b1ca9b37b476579b3
Kristers Laganovskis <58227978+kristersla@users.noreply.github.com>
2026-01-04 02:29:47 +0200

    Add files via upload

--- server/server.py
+++ server/server.py
@@ -204,7 +204,7 @@ async def handler(websocket):
 
 
 async def main():
-    async with websockets.serve(handler, "0.0.0.0", 8080):
+    async with websockets.serve(handler, "0.0.0.0", 8765):
         await asyncio.Future()
 
 
